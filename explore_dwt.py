f953f02526fdf228200312d7e7df703901c36058
Arif <arif@themascoteers.com>
2023-11-10 04:55:25 +1100

    done

--- explore_dwt.py
+++ explore_dwt.py
@@ -2,15 +2,15 @@ import pandas as pd
 import pywt
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data/dataset_r.csv")
+df = pd.read_csv("data/dataset_min.csv")
 signal = df.iloc[0].to_numpy()
 start_index = list(df.columns).index("400")
 signal = signal[start_index:]
-# plt.plot(signal)
-# plt.show()
+plt.plot(signal)
+plt.show()
 
 signal,_,_,_,_,_,_ = pywt.wavedec(signal, 'db1', level=6)
 print(signal.shape)
-# plt.plot(signal)
-# plt.show()
+plt.plot(signal)
+plt.show()
 
