65d04423a36817500b209d29099d59b39324c3c7
Arif <arif@themascoteers.com>
2023-11-10 04:33:34 +1100

    done

--- pre_shorten_columns.py
+++ pre_shorten_columns.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import numpy as np
 import pywt
-import matplotlib.pyplot as plt
-from scipy.interpolate import CubicSpline
 
 df = pd.read_csv("data/dataset_r.csv")
 start_col = list(df.columns).index("400")
@@ -10,4 +7,13 @@ df2 = df.iloc[:,0:start_col]
 nrows = len(df2)
 for i in range(66):
     df2.insert(len(df2.columns), f"{i}", pd.Series([0]*nrows))
+
+
+start_col = list(df.columns).index("0")
+for i in range(nrows):
+    signal = df.iloc[i, start_col:]
+    signal, _, _, _, _, _, _ = pywt.wavedec(signal, 'db1', level=6)
+    df2.iloc[i, start_col:] = signal
+
+
 df2.to_csv("data/dataset_66.csv", index=False)
