6b2e0e5eba420ba7684698b2ac0c1318483e188e
Arif <arif@themascoteers.com>
2023-11-10 04:27:10 +1100

    done

--- pre_shorten_columns.py
+++ pre_shorten_columns.py
@@ -10,4 +10,4 @@ df2 = df.iloc[:,0:start_col]
 nrows = len(df2)
 for i in range(66):
     df2.insert(len(df2.columns), f"{i}", pd.Series([0]*nrows))
-df2.to_csv("data/dataset_66.csv", ind)
+df2.to_csv("data/dataset_66.csv", index=False)
