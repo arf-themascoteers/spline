222033b94e8f4cf52b0f837e5066281b9ec74bb4
arif <arif@themascoteers.com>
2023-11-12 16:49:11 +1100

    done

--- pre_shorten_rows.py
+++ pre_shorten_rows.py
@@ -3,3 +3,7 @@ import pandas as pd
 df = pd.read_csv("data/dataset_r.csv")
 df = df.sample(frac=0.01)
 df.to_csv("data/dataset_min.csv", index=False)
+
+df = pd.read_csv("data/dataset_66.csv")
+df = df.sample(frac=0.01)
+df.to_csv("data/dataset_66_min.csv", index=False)
