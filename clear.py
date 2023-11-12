49d12445e7ac61bcb8247c3ccc738de3cab44748
arif <arif@themascoteers.com>
2023-11-12 16:52:51 +1100

    done

--- clear.py
+++ clear.py
@@ -3,10 +3,7 @@ import os
 
 
 def clear_all():
-    shutil.rmtree("checkpoints")
-    shutil.rmtree("lightning_logs")
-    shutil.rmtree("results")
-
-    os.mkdir("checkpoints")
-    os.mkdir("lightning_logs")
-    os.mkdir("results")
+    for folder in ["checkpoints", "lightning_logs", "results"]:
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+            os.mkdir(folder)
