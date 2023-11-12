8cc1ff10bbc5802509d1c1247347ad7c42507fca
arif <arif@themascoteers.com>
2023-11-12 16:50:49 +1100

    done

--- constants.py
+++ constants.py
@@ -1,14 +1,9 @@
 import os
 
-TEST = False
+TEST = True
 LUCAS_PATH = r"D:\Data\LUCAS\Lucas-2015"
 DATASET = "data/dataset_66.csv"
-SPECTRA_DIR = os.path.join(LUCAS_PATH,"LUCAS2015_Soil_Spectra_EU28")
-if os.path.exists(SPECTRA_DIR):
-    SPECTRA_FILES = [os.path.join(SPECTRA_DIR, file) for file in os.listdir(SPECTRA_DIR)]
-TOPSOIL_FILE = os.path.join(os.path.join(LUCAS_PATH, "LUCAS2015_topsoildata_20200323"),"LUCAS_Topsoil_2015_20200323.csv")
 LIGHTNING = False
 
 if TEST:
-    SPECTRA_FILES = [os.path.join(SPECTRA_DIR, "spectra_ MT .csv")]
-    DATASET = "data/dataset_min.csv"
+    DATASET = "data/dataset_66_min.csv"
